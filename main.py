05a4d576cbb86b287e14b56c9934e23257b8ad23
GesikHub <31306248+GesikHub@users.noreply.github.com>
2017-09-25 09:59:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -198,6 +198,6 @@ try:
                 return user
 
     if __name__ == '__main__':
-        bot.polling(none_stop=True)
+        bot.polling(none_stop=False, interval=0, timeout=20)
 except:
     bot.send_message(admin[1], "Я упал, помоги мне")
