23c4b33029a3c7a4d6738f28f5fda2ca44e7da6c
GesikHub <edikaleks43@gmail.com>
2017-09-30 22:36:39 +0300

    server1.2.3

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import telebot
 import data.buttons
 import myString
+import dj_database_url
+import psycopg2
 import os
 from flask import Flask, request
 from config import TOKEN
@@ -14,6 +16,26 @@ server = Flask(__name__)
 check_message = False
 admin_mesage = ''
 students = []
+
+DATABASELINK = "postgres://icuuqvetjcyawb:e2563bd5ad0b32be65b13a1c7556571cfd322eff1ee571e73dc06a904252ae77@ec2-54-228-182-57.eu-west-1.compute.amazonaws.com:5432/d9mad9tbhdd8i9"
+
+db_info = dj_database_url.config(default=DATABASELINK)
+
+conn = psycopg2.connect(database=db_info.get('NAME'),
+		    		user=db_info.get('USER'),
+		    		password=db_info.get('PASSWORD'),
+		    		host=db_info.get('HOST'),
+		    		port=db_info.get('PORT'))
+
+cursor = conn.cursor()
+newtable = "CREATE TABLE students (" \
+           "id INTEGER, " \
+           "first_name TEXT, " \
+           "second_name TEXT, " \
+           "gruop_number TEXT, " \
+           "PRIMARY KEY (id)" \
+           ")"
+cursor.execute(newtable)
 
 try:
     @bot.message_handler(commands=['start'])
