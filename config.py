6196d65d284ae0fbaf9c2bdf8cc9f123d6446a1c
GesikHub <edikaleks43@gmail.com>
2017-10-09 22:46:01 +0300

    time_to_session

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-TOKEN = "413055169:AAEFwg2vGyprcAWzGE5x1gXhyHeZxCLHGhk"
+TOKEN = "439253618:AAEC3C29rePF6ZdREKJfOv32zw3T_TjJPhg"
 
 database_name = "postgres://icuuqvetjcyawb:e2563bd5ad0b32be65b13a1c7556571cfd322eff1ee571e73dc06a904252ae77@ec2" \
                 "-54-228-182-57.eu-west-1.compute.amazonaws.com:5432/d9mad9tbhdd8i9"
